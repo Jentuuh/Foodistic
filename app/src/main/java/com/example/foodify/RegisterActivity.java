f6f8328ff06cbba0f9b6c17ebf976b8f70b0621d
jente-student <jente.vandersanden@student.uhasselt.be>
2020-04-28 14:00:47 +0200

    Finished RegisterActivity, created LoginActivity

---
+++ app/src/main/java/com/example/foodify/RegisterActivity.java
@@ -0,0 +1,133 @@
+package com.example.foodify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button m_login_button;
+    private Button m_register_button;
+    private EditText m_first_name;
+    private EditText m_last_name;
+    private EditText m_email;
+    private EditText m_address;
+    private EditText m_password;
+    private EditText m_confirm_password;
+
+    /**
+     * @author jentevandersanden
+     * This class represents the first window (login/register) that will be presented in the application.
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        m_register_button = (Button) findViewById(R.id.registerButton);
+        m_login_button = (Button) findViewById(R.id.loginButton);
+        m_first_name = (EditText) findViewById(R.id.firstName);
+        m_last_name = (EditText) findViewById(R.id.lastName);
+        m_email = (EditText) findViewById(R.id.email);
+        m_address = (EditText) findViewById(R.id.address);
+        m_password = (EditText) findViewById(R.id.password);
+        m_confirm_password = (EditText) findViewById(R.id.passwordConfirm);
+
+
+        m_register_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onRegisterClick();
+            }
+        });
+
+        m_login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLoginClick();
+            }
+        });
+    }
+
+
+    /**
+     * Callback method that handles what needs to happen when the register button is clicked.
+     */
+    public void onRegisterClick(){
+        if(checkPasswords() && checkFilledIn()){
+            // Create user
+            insertIntoDataBase((m_first_name.getText()).toString(), (m_last_name.getText()).toString(), (m_email.getText()).toString() , (m_address.getText()).toString(), (m_password.getText()).toString(), (m_confirm_password.getText()).toString());
+            // Log in
+            login(m_email.getText().toString(), m_password.getText().toString());
+        }
+
+    }
+
+    /**
+     * Callback method that handles what needs to happen when the login button is clicked.
+     */
+    public void onLoginClick(){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Method that checks if password and confirm_password match.
+     * @return boolean
+     */
+    public boolean checkPasswords(){
+        if(m_password.getText().toString().matches(m_confirm_password.getText().toString())){
+            return true;
+        }
+        else {
+            // Display a short toast to give feedback to the user that the passwords didn't match.
+            Toast.makeText(this, "Zorg ervoor dat de paswoorden overeen komen.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    /**
+     * Method that checks whether all text fields were filled in.
+     * @return boolean
+     */
+    public boolean checkFilledIn(){
+        if(!(m_first_name.getText().toString().matches("") || m_last_name.getText().toString().matches("") || m_address.getText().toString().matches("") || m_password.getText().toString().matches("") || m_confirm_password.getText().toString().matches("") || m_email.getText().toString().matches(""))){
+            return true;
+        }
+        else{
+            // Display a short toast to give feedback to the user that they didn't fill in every textfield.
+            Toast.makeText(this, "Vul eerst alle velden in", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+    /**
+     * Method that inserts the register data into the database. (Creates a user account)
+     * @param firstname
+     * @param lastname
+     * @param email
+     * @param address
+     * @param password
+     * @param confirmpassword
+     */
+    public boolean insertIntoDataBase(String firstname, String lastname, String email, String address, String password, String confirmpassword){
+        // TODO: implement
+        return true;
+    }
+
+    /**
+     * Method that logs the user in.
+     */
+    public void login(String email, String password){
+        // TODO: implement
+    }
+
+
+}
