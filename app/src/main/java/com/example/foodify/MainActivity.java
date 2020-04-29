6ad8f882ffa588bf12298535f1f0c01be4cecea9
TimLukasBlom <57345138+TimLukasBlom@users.noreply.github.com>
2020-04-29 17:42:36 +0200

    icons added

--- app/src/main/java/com/example/foodify/MainActivity.java
+++ app/src/main/java/com/example/foodify/MainActivity.java
@@ -6,9 +6,13 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.ClipData;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -19,9 +23,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winkel);
+
         // setting up navigation controller
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
         NavigationUI.setupWithNavController(bottomNav, navController);
+
+        // Use of icons
+        Typeface iconFont = FontManager.getTypeface(getApplicationContext(), FontManager.FONTAWESOME);
+       // (findViewById(R.id.shopFragment)).setTypeface(iconFont);
+
+
     }
 }
--- app/src/main/java/com/example/foodify/RegisterActivity.java
+++ app/src/main/java/com/example/foodify/RegisterActivity.java
@@ -29,7 +29,7 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
         m_register_button = (Button) findViewById(R.id.registerButton);
         m_login_button = (Button) findViewById(R.id.loginButton);
