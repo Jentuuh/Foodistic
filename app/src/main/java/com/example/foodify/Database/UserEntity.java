8ec55069486776faba36b139d4b65bcda8049c06
TimLukasBlom <tim-lukas.blom@student.uhasselt.be>
2020-05-04 17:08:57 +0200

    toolbar fixed

--- app/src/main/java/com/example/foodify/Database/UserEntity.java
+++ app/src/main/java/com/example/foodify/Database/UserEntity.java
@@ -1,4 +1,11 @@
 package com.example.foodify.Database;
 
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class UserEntity {
+    @PrimaryKey @NonNull
+    public String primKey;
 }
--- app/src/main/java/com/example/foodify/Database/foodisticDAO.java
+++ app/src/main/java/com/example/foodify/Database/foodisticDAO.java
@@ -1,4 +1,7 @@
 package com.example.foodify.Database;
 
+import androidx.room.Dao;
+
+@Dao
 public interface foodisticDAO {
 }
