f74c3d132f59bd44719360e84fd5647590f71af3
Joris <joris.bertram@student.uhasselt.be>
2020-05-10 18:19:11 +0200

    Before merge

--- app/src/main/java/com/example/foodify/ProfileFragment.java
+++ app/src/main/java/com/example/foodify/ProfileFragment.java
@@ -2,17 +2,11 @@ package com.example.foodify;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-import androidx.annotation.DimenRes;
-import androidx.annotation.DrawableRes;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
